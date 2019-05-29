5ff49806362f73a6e04e3e3287408c2f49f61899
marcwadai <msiri.studio@gmail.com>
2019-05-29 12:24:55 +0800

    create comments link to user

--- functions/create_comments/index.js
+++ functions/create_comments/index.js
@@ -3,14 +3,16 @@ const util = require('./utils/util');
 
 const TABLE_COMMENT = 'events-comments';
 const TABLE_EVENTS = 'events-list';
+const TABLE_USERS = 'users-list';
 
-function buildParams(bodyRaw, tableName) {
+function buildParams(bodyRaw, user, tableName) {
     const body = JSON.parse(bodyRaw);
     return {
         TableName: tableName,
         Item: {
             event_id: body.event_id,
-            user_id: util.generateUuid(),
+            user_id: user.user_id,
+            username: user.username,
             comment_id: util.generateUuid(),
             timestamp: util.getCurrrentTimestampSeconde(),
             message: body.message,
@@ -18,7 +20,7 @@ function buildParams(bodyRaw, tableName) {
     };
 }
 
-function buildParamsGet(eventId, tableName) {
+function buildParamsGetEvent(eventId, tableName) {
     return {
         TableName: tableName,
         Key: {
@@ -27,9 +29,18 @@ function buildParamsGet(eventId, tableName) {
     };
 }
 
+function buildParamsGetUser(userId, tableName) {
+    return {
+        TableName: tableName,
+        Key: {
+            user_id: userId,
+        },
+    };
+}
+
 
 async function checkEventExist(eventId) {
-    const eventFound = await dynamodb.getItem(buildParamsGet(eventId, TABLE_EVENTS));
+    const eventFound = await dynamodb.getItem(buildParamsGetEvent(eventId, TABLE_EVENTS));
     console.log(eventFound);
     if (Object.prototype.hasOwnProperty.call(eventFound, 'Item')) {
         return true;
@@ -37,20 +48,30 @@ async function checkEventExist(eventId) {
     return false;
 }
 
+
+async function checkUserExist(userId) {
+    const userFound = await dynamodb.getItem(buildParamsGetUser(userId, TABLE_USERS));
+    console.log(userFound);
+    if (Object.prototype.hasOwnProperty.call(userFound, 'Item')) {
+        return userFound.Item;
+    }
+    return null;
+}
+
 module.exports.handler = async (event, context, callback) => {
     try {
         const body = JSON.parse(event.body);
         console.log('body', body); // Contains incoming request data (e.g., query params, headers and more)
-        const eventExist = await checkEventExist(body.event_id);
-        if (eventExist) {
+        const [eventExist, user] = await Promise.all([checkEventExist(body.event_id), checkUserExist(body.user_id)]);
+        if (eventExist && user) {
             // we insert the event in the event and user event list
             // we insert the event in the event and user event list
-            const newCommentCreated = await dynamodb.createItem(buildParams(event.body, TABLE_COMMENT));
+            const newCommentCreated = await dynamodb.createItem(buildParams(event.body, user, TABLE_COMMENT));
             console.log('result', newCommentCreated);
             callback(null, util.buildResp(200, newCommentCreated));
             return;
         }
-        callback(null, util.buildResp(404, `The event ${body.event_id} was not found`));
+        callback(null, util.buildResp(404, `The event ${body.event_id} or user was not found`));
     } catch (err) {
         console.error(err);
         callback(null, util.buildResp(500, err));
